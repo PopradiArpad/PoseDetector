9c0d6becd144b1123d17e68328887494d7e04fb7
Árpád Poprádi <arpad@popradiarpad.com>
2025-10-28 18:51:07 +0100

    Fix missing iosApp. Fix dummy iOS app (fix screen instead of live vide). Gitignore XCode user data. (#15)
    * Copy-paste an iosAPP folder from a fresh KMP project because we somehow don't have that.
    
    * Adapt the copied iosApp Swift code to use the shared/iosMain bridge (from Compose to UIKit).
    
    * Fix missing actual for iOS.
    
    * Gitignore xcode user data, take them out of version control.

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,18 @@
+import UIKit
+import SwiftUI
+import shared
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        return MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+struct ContentView: View {
+    var body: some View {
+        ComposeView()
+            .ignoresSafeArea(.all) // Use this to extend to the edges
+    }
+}
