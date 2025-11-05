d8a3e18583e01e21c5183d92ea79365ddd04f537
Árpád Poprádi <arpad@popradiarpad.com>
2025-11-05 20:08:20 +0100

    Simplify LivePoseLandmarkerBackground's SwiftUI integration into Compose. (#19)
    * Simplify LivePoseLandmarkerBackground's SwiftUI integration into Compose.
    
    * Explain the mechanism more.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -3,8 +3,14 @@ import shared
 
 @main
 struct iOSApp: App {
+    // Integrating SwiftUI into Compose needs one step more
+    // then the other way around because
+    // shared doesn't see anything from iosApp,
+    // but iosApp sees everything from shared.
     init() {
-        LivePoseLandmarkerScreenFactoryProvider.shared.factory = LivePoseLandmarkerScreenFactoryImpl()
+        LivePoseLandmarkerBackgroundFactory.shared.factory = {
+            return UIHostingController(rootView: LivePoseLandmarkerScreenIos())
+        }
     }
 
     var body: some Scene {
--- shared/src/iosMain/kotlin/com/popradiarpad/example/posedetector/shared/ui/screen/LivePoseLandmarkerBackground.kt
+++ shared/src/iosMain/kotlin/com/popradiarpad/example/posedetector/shared/ui/screen/LivePoseLandmarkerBackground.kt
@@ -4,19 +4,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.UIKitViewController
 import kotlinx.cinterop.ExperimentalForeignApi
+import platform.UIKit.UIViewController
 
 @OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun LivePoseLandmarkerBackground(
     modifier: Modifier,
 ) {
-    // This factory is now simpler as it doesn't need the onFinish callback
-    val factory = {
-        LivePoseLandmarkerScreenFactoryProvider.factory.create()// { /* The finish button is now in common code */ }
-    }
-
     UIKitViewController(
-        factory = factory,
+        factory = LivePoseLandmarkerBackgroundFactory.factory,
         modifier = modifier,
     )
+}
+
+// We want integrate a SwiftUI component into Composable and
+// iosApp things are not visible from shared (only the other way around)
+// so we need storage which is
+// 1. visible from both world (so it must be defined in shared, here)
+// 2. can be set from iosApp
+// 3. can be read from shared.
+object LivePoseLandmarkerBackgroundFactory {
+    lateinit var factory: () -> UIViewController
 }
