9ab227ade9fcca6cc9406998c0f335c9213f9cb4
Poprádi Árpád <arpad@popradiarpad.com>
2025-11-05 19:55:48 +0100

    Simplify LivePoseLandmarkerBackground's SwiftUI integration into Compose.

--- shared/src/iosMain/kotlin/com/popradiarpad/example/posedetector/shared/ui/screen/LivePoseLandmarkerBackground.kt
+++ shared/src/iosMain/kotlin/com/popradiarpad/example/posedetector/shared/ui/screen/LivePoseLandmarkerBackground.kt
@@ -4,19 +4,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.UIKitViewController
 import kotlinx.cinterop.ExperimentalForeignApi
+import platform.UIKit.UIViewController
 
 @OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun LivePoseLandmarkerBackground(
     modifier: Modifier,
 ) {
-    // This factory is now simpler as it doesn't need the onFinish callback
-    val factory = {
-        LivePoseLandmarkerScreenFactoryProvider.factory.create()// { /* The finish button is now in common code */ }
-    }
-
     UIKitViewController(
-        factory = factory,
+        factory = LivePoseLandmarkerBackgroundFactory.factory,
         modifier = modifier,
     )
+}
+
+object LivePoseLandmarkerBackgroundFactory {
+    lateinit var factory: () -> UIViewController
 }
