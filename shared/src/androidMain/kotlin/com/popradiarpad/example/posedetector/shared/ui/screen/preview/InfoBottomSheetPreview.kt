12e748fb91146e81d3c5daf16b66969bb3669267
Poprádi Árpád <arpad@popradiarpad.com>
2025-11-06 15:53:23 +0100

    Add fake InferenceTime to InfoBottomSheet.

---
+++ shared/src/androidMain/kotlin/com/popradiarpad/example/posedetector/shared/ui/screen/preview/InfoBottomSheetPreview.kt
@@ -0,0 +1,16 @@
+package com.popradiarpad.example.posedetector.shared.ui.screen.preview
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.popradiarpad.example.posedetector.shared.ui.screen.InfoBottomSheet
+import com.popradiarpad.example.posedetector.shared.ui.screen.LivePoseLandmarkerScreen
+import com.popradiarpad.example.posedetector.shared.ui.theme.AppTheme
+
+@Preview
+@Composable
+fun InfoBottomSheetPreview() {
+    AppTheme(darkTheme = isSystemInDarkTheme(), dynamicColor = false) {
+        InfoBottomSheet()
+    }
+}
