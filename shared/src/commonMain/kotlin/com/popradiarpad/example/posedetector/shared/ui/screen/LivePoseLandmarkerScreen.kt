d92976bead7b09178b13bf8464235857324e8ca8
Poprádi Árpád <arpad@popradiarpad.com>
2025-11-06 13:12:03 +0100

    LivePoseLandmarkerScreen: Add buttons info and close.

--- shared/src/commonMain/kotlin/com/popradiarpad/example/posedetector/shared/ui/screen/LivePoseLandmarkerScreen.kt
+++ shared/src/commonMain/kotlin/com/popradiarpad/example/posedetector/shared/ui/screen/LivePoseLandmarkerScreen.kt
@@ -2,7 +2,7 @@ package com.popradiarpad.example.posedetector.shared.ui.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
@@ -11,10 +11,14 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LivePoseLandmarkerScreen(
@@ -27,22 +31,47 @@ fun LivePoseLandmarkerScreen(
         LivePoseLandmarkerBackground(
             modifier = Modifier.fillMaxSize()
         )
-        BackButton(onFinish)
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(8.dp),
+        ) {
+            InfoButton(onClick = { print("Info") })
+            BackButton(onFinish)
+        }
     }
 }
+
 @Composable
-private fun BoxScope.BackButton(onFinish: () -> Unit) {
+private fun BackButton(onClick: () -> Unit) {
     IconButton(
-        onClick = onFinish,
+        onClick = onClick,
         modifier = Modifier
-            .align(Alignment.BottomStart)
-            .padding(16.dp)
+            .padding(8.dp)
             .background(MaterialTheme.colorScheme.surface, CircleShape),
     ) {
         Icon(
             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
             contentDescription = "Back",
             tint = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
+
+@Composable
+private fun InfoButton(onClick: () -> Unit) {
+    IconButton(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(8.dp)
+            .background(MaterialTheme.colorScheme.surface, CircleShape),
+    ) {
+        Text(
+            text = "i",
+            color = MaterialTheme.colorScheme.onSurface,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            fontSize = 30.sp
         )
     }
 }
