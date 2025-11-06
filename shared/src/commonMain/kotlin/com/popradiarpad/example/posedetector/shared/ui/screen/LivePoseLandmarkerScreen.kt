7020d5e2ef8428ee2c371a815e98489deb762bf7
Poprádi Árpád <arpad@popradiarpad.com>
2025-11-06 15:16:44 +0100

    Use standard bottom sheet instead of modal: more natural.

--- shared/src/commonMain/kotlin/com/popradiarpad/example/posedetector/shared/ui/screen/LivePoseLandmarkerScreen.kt
+++ shared/src/commonMain/kotlin/com/popradiarpad/example/posedetector/shared/ui/screen/LivePoseLandmarkerScreen.kt
@@ -10,19 +10,15 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
@@ -31,57 +27,51 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LivePoseLandmarkerScreen(
     modifier: Modifier = Modifier,
     onFinish: () -> Unit
 ) {
-    var showBottomSheet by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState()
 
-    Box(
-        modifier = modifier.fillMaxSize(),
-    ) {
-        LivePoseLandmarkerBackground(
-            modifier = Modifier.fillMaxSize()
-        )
-        Column(
-            modifier = Modifier
-                .align(Alignment.BottomStart)
-                .padding(8.dp),
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 0.dp, // When closed, nothing to see from the bottom sheet
+        sheetContent = {
+            InfoBottomSheet()
+        }) {
+        Box(
+            modifier = modifier.fillMaxSize(),
         ) {
-            InfoButton(onClick = { showBottomSheet = true })
-            BackButton(onFinish)
-        }
-
-        if (showBottomSheet) {
-            InfoBottomSheet(onDismiss = { showBottomSheet = false })
+            LivePoseLandmarkerBackground(
+                modifier = Modifier.fillMaxSize()
+            )
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(8.dp),
+            ) {
+                InfoButton(onClick = {
+                    scope.launch {
+                        // No programmatic closing: just swipe down
+                        scaffoldState.bottomSheetState.expand()
+                    }
+                })
+                BackButton(onFinish)
+            }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun InfoBottomSheet(onDismiss: () -> Unit) {
-    val sheetState = rememberModalBottomSheetState()
-    val scope = rememberCoroutineScope()
-    ModalBottomSheet(
-        onDismissRequest = onDismiss,
-        sheetState = sheetState
-    ) {
-        Box(modifier = Modifier.fillMaxWidth()) {
-            IconButton(
-                onClick = {
-                    scope.launch { sheetState.hide() }.invokeOnCompletion {
-                        if (!sheetState.isVisible) {
-                            onDismiss()
-                        }
-                    }
-                },
-                modifier = Modifier.align(Alignment.TopStart)
-            ) {
-                Icon(Icons.Default.Close, contentDescription = "Close sheet")
-            }
-        }
+private fun InfoBottomSheet() {
+    Box(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+        Text(
+            "This is the info content.",
+            modifier = Modifier.padding(top = 48.dp).align(Alignment.Center)
+        )
     }
 }
 
