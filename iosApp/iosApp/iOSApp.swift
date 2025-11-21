735f38093e8e7916d71018a6cd928cef40677447
Poprádi Árpád <arpad@popradiarpad.com>
2025-11-21 18:47:35 +0100

    iOS app: Fix life cycle usage by using the lib defined platform independent one instead of the defunc own implementation.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -3,6 +3,9 @@ import shared
 
 @main
 struct iOSApp: App {
+    // @UIApplicationDelegateAdaptor(AppDelegate.self)
+    // var appDelegate: AppDelegate
+
     // Integrating SwiftUI into Compose needs one step more
     // then the other way around because
     // shared doesn't see anything from iosApp,
@@ -20,3 +23,28 @@ struct iOSApp: App {
         }
     }
 }
+
+// class AppDelegate: NSObject, UIApplicationDelegate {
+//     private var stateKeeper = StateKeeperDispatcherKt.StateKeeperDispatcher(savedState: nil)
+//
+//     lazy var root: RootComponent = DefaultRootComponent(
+//         componentContext: DefaultComponentContext(
+//             lifecycle: ApplicationLifecycle(),
+//             stateKeeper: stateKeeper,
+//             instanceKeeper: nil,
+//             backHandler: nil
+//         ),
+//         featureInstaller: DefaultFeatureInstaller.shared,
+//         deepLinkUrl: nil
+//     )
+//
+//     func application(_ application: UIApplication, shouldSaveSecureApplicationState coder: NSCoder) -> Bool {
+//         StateKeeperUtilsKt.save(coder: coder, state: stateKeeper.save())
+//         return true
+//     }
+//
+//     func application(_ application: UIApplication, shouldRestoreSecureApplicationState coder: NSCoder) -> Bool {
+//         stateKeeper = StateKeeperDispatcherKt.StateKeeperDispatcher(savedState: StateKeeperUtilsKt.restore(coder: coder))
+//         return true
+//     }
+// }
--- shared/src/iosMain/kotlin/com/popradiarpad/example/posedetector/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/popradiarpad/example/posedetector/shared/MainViewController.kt
@@ -3,15 +3,14 @@ package com.popradiarpad.example.posedetector.shared
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
-import com.popradiarpad.example.posedetector.shared.lifecycle.LifecycleOwner
 import com.popradiarpad.example.posedetector.shared.ui.App
 import com.popradiarpad.example.posedetector.shared.ui.component.RootComponent
+import com.arkivanov.essenty.lifecycle.ApplicationLifecycle
 
 fun MainViewController() = ComposeUIViewController {
-    val lifecycle = remember { LifecycleOwner() }
     val root = remember {
         RootComponent(
-            componentContext = DefaultComponentContext(lifecycle.lifecycle)
+            componentContext = DefaultComponentContext(lifecycle = ApplicationLifecycle())
         )
     }
     App(root)
