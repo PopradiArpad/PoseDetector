5a8b15f8dd8bf6fc48504e44e7ac96230dfe75c0
Poprádi Árpád <arpad@popradiarpad.com>
2025-12-04 09:33:50 +0100

    Store the last 10s inferenceTime in a deque.

--- shared/src/commonMain/kotlin/com/popradiarpad/example/posedetector/shared/viewmodel/InferenceTimeStorage.kt
+++ shared/src/commonMain/kotlin/com/popradiarpad/example/posedetector/shared/viewmodel/InferenceTimeStorage.kt
@@ -2,16 +2,30 @@ package com.popradiarpad.example.posedetector.shared.viewmodel
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlin.time.Clock.*
+import kotlin.time.ExperimentalTime
 
 // Singleton to be a data storage between platform specific modules and common UI.
 object InferenceTimeStorage {
+    data class DataPoint(val inferenceTimeMs: Double, val timestampEpochMs: Long)
+
     // Data read in common UI
     private val _inferenceTimeMs: MutableStateFlow<Double?> = MutableStateFlow(null)
     val inferenceTimeMs = _inferenceTimeMs.asStateFlow()
 
+    private val dataPoints = ArrayDeque<DataPoint>()
+
     // Data is set in the platform specific modules
+    @OptIn(ExperimentalTime::class)
     fun setInferenceTimeMs(value: Double) {
         _inferenceTimeMs.value = value
-    }
 
+        val now = System.now().toEpochMilliseconds()
+        dataPoints.addFirst(DataPoint(inferenceTimeMs = value, timestampEpochMs = now))
+
+        val tenSecondsAgo = now - 10_000
+        while (dataPoints.isNotEmpty() && dataPoints.last().timestampEpochMs < tenSecondsAgo) {
+            dataPoints.removeLast()
+        }
+    }
 }
